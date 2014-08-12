05a6106cdfe686bb4f6ca66e1e63533bab75dcf9
aviolette <aviolette@gmail.com>
2014-08-12 17:11:31 -0500

    Added page titles and group therapy manuals

--- drthuypham.rb
+++ drthuypham.rb
@@ -1,25 +1,31 @@
 require 'sinatra'
 
 get '/' do
+  @title = 'Thuy Pham'
   erb :index
 end
 
 get '/fees' do
+  @title = 'Fees'
   erb :fees
 end
 
 get '/forms' do
+  @title = 'Forms'
   erb :forms
 end
 
 get '/services' do
+  @title = 'Services'
   erb :services
 end
 
 get '/why_therapy' do
+  @title = 'Why Therapy'
   erb :why_therapy
 end
 
 get '/contact' do
+  @title = 'Contact'
   erb :contact
 end
